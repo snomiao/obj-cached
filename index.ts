3c8687ba54720b35743e10af7168a7c5d2c01f4e
snomiao <snomiao@gmail.com>
2024-04-04 05:33:18 +0000

    fix(main): md5
    md5

--- index.ts
+++ index.ts
@@ -1,9 +1,10 @@
+import md5 from 'md5'
 export function objCached<Args extends unknown[], Result>(
   fn: (...args: Args) => Result,
   obj: Record<string, unknown> = globalThis,
   prefix = "obj-cached-"
 ) {
-  const key = `${prefix}${fn.toString()}`;
+  const key = md5(`${prefix}${fn.toString()}`);
   return (...args: Args) => (obj[`${key}@${JSON.stringify(args)}`] ??= fn(...args)) as Result;
 }
 export function objCachedAsync<Args extends unknown[], Result>(
@@ -11,7 +12,7 @@ export function objCachedAsync<Args extends unknown[], Result>(
   obj: Record<string, Promise<unknown>|unknown> = globalThis,
   prefix = "obj-cached-"
 ) {
-  const key = `${prefix}${fn.toString()}`;
+  const key = md5(`${prefix}${fn.toString()}`);
   return async (...args: Args) => {
     const cachedResult = await obj[`${key}@${JSON.stringify(args)}`]
     return await (obj[`${key}@${JSON.stringify(args)}`] = cachedResult ?? await fn(...args)) as Result
